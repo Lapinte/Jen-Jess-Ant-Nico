4d554dceb557941339db202f34d2310655d3474c
Jessy <jessy@laniel.me>
2018-07-25 17:18:33 +0200

    Ajout AgenciesController avec vues, Ajout des nugets JavaScript, Bootstraps, Ajout de CustomersControllers, et HomeController

---
+++ BoVoyageJJAN/BoVoyageJJAN/Areas/BackOffice/Controllers/AgenciesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageJJAN.Data;
+using BoVoyageJJAN.Models;
+
+namespace BoVoyageJJAN.Areas.BackOffice.Controllers
+{
+    public class AgenciesController : Controller
+    {
+        private JjanDbContext db = new JjanDbContext();
+
+        // GET: BackOffice/Agencies
+        public ActionResult Index()
+        {
+            return View(db.Agencies.ToList());
+        }
+
+        // GET: BackOffice/Agencies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agency agency = db.Agencies.Find(id);
+            if (agency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agency);
+        }
+
+        // GET: BackOffice/Agencies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BackOffice/Agencies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name")] Agency agency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agencies.Add(agency);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agency);
+        }
+
+        // GET: BackOffice/Agencies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agency agency = db.Agencies.Find(id);
+            if (agency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agency);
+        }
+
+        // POST: BackOffice/Agencies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name")] Agency agency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agency).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agency);
+        }
+
+        // GET: BackOffice/Agencies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agency agency = db.Agencies.Find(id);
+            if (agency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agency);
+        }
+
+        // POST: BackOffice/Agencies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agency agency = db.Agencies.Find(id);
+            db.Agencies.Remove(agency);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
