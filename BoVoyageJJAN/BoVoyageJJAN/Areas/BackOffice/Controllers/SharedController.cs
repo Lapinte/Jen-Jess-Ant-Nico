8d07f80a0464270996a564e49ef7eb41e8cbbb12
Nico <admin@admin.com>
2018-07-26 13:01:40 +0200

    Modif model reservation

---
+++ BoVoyageJJAN/BoVoyageJJAN/Areas/BackOffice/Controllers/SharedController.cs
@@ -0,0 +1,20 @@
+﻿using BoVoyageJJAN.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageJJAN.Areas.BackOffice.Controllers
+{
+    public class SharedController : BaseController
+    {
+        // GET: BackOffice/SharedBO
+        [ChildActionOnly]
+        public ActionResult PendingReservation()
+        {
+            var pendingRersevation = db.Reservations.Where(x => x.Statut == 0).ToList();
+            return View("_PendingReservation", pendingRersevation);
+        }
+    }
+}
