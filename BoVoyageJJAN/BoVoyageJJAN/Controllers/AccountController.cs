fe23c7c6258f1705d61755b654c52ef4a5507e69
Antoine <antoine_352@hotmail.fr>
2018-07-26 11:24:17 +0200

    Remplacement du CustomerController par un AccountController

---
+++ BoVoyageJJAN/BoVoyageJJAN/Controllers/AccountController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageJJAN.Data;
+using BoVoyageJJAN.Filter;
+using BoVoyageJJAN.Models;
+using BoVoyageJJAN.Utils;
+
+namespace BoVoyageJJAN.Controllers
+{
+    public class AccountController : BaseController
+    {
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            var customers = db.Customers.Include(c => c.Civility);
+            return View(customers.ToList());
+        }
+
+        // GET: Accounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        
+        // GET: Accounts/Create
+        public ActionResult Create()
+        {
+            ViewBag.CivilityID = new SelectList(db.Civilities, "ID", "Label");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Mail,Password,ConfirmedPassword,Lastname,Firstname,Address,Phone,BirthDate,CivilityID")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Configuration.ValidateOnSaveEnabled = false;
+                customer.Password = customer.Password.HashMD5();
+
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CivilityID = new SelectList(db.Civilities, "ID", "Label", customer.CivilityID);
+            return View(customer);
+        }
+
+        // GET: Accounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CivilityID = new SelectList(db.Civilities, "ID", "Label", customer.CivilityID);
+            return View(customer);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Mail,Password,Lastname,Firstname,Address,Phone,BirthDate,CivilityID")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CivilityID = new SelectList(db.Civilities, "ID", "Label", customer.CivilityID);
+            return View(customer);
+        }
+
+        // GET: Accounts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
