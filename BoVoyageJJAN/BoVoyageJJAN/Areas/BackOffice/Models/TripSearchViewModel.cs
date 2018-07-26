3b46629d772a0962d5521c15b553921dbf917234
Jennifer <Adminl@EMER05>
2018-07-26 19:50:37 +0200

    Méthode search toujours en cours.

--- BoVoyageJJAN/BoVoyageJJAN/Areas/BackOffice/Models/TripSearchViewModel.cs
+++ BoVoyageJJAN/BoVoyageJJAN/Areas/BackOffice/Models/TripSearchViewModel.cs
@@ -3,6 +3,7 @@ using BoVoyageJJAN.Utils.Validator;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,22 +12,23 @@ namespace BoVoyageJJAN.Areas.BackOffice.Models
     public class TripSearchViewModel
     {
         [Display(Name = "Destination")]
+        
         [StringLength(50, ErrorMessage = "Destination trop longue")]
-        public string Destination { get; set; }
+        public Destination Destination { get; set; }
 
         [Display(Name = "Prix maximum")]
-        public decimal MaxPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
 
         [Display(Name = "Prix minimum")]
-        public decimal MinPrice { get; set; }
+        public decimal? MinPrice { get; set; }
 
         [Display(Name = "Départ après le")]
         [DataType(DataType.Date)]
-        public DateTime MinDate { get; set; }
+        public DateTime? MinDate { get; set; }
 
         [Display(Name = "Retour avant le ")]
         [DataType(DataType.Date)]
-        public DateTime MaxDate { get; set; }
+        public DateTime? MaxDate { get; set; }
 
         public IEnumerable<Trip> Trips { get; set; }
     }
