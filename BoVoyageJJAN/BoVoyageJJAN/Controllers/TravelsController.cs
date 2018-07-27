4309c755e34231e35a545ea9b6e05d4388734159
Nico <admin@admin.com>
2018-07-27 12:44:46 +0200

    Recherche des travels en front office

--- BoVoyageJJAN/BoVoyageJJAN/Controllers/TravelsController.cs
+++ BoVoyageJJAN/BoVoyageJJAN/Controllers/TravelsController.cs
@@ -14,7 +14,7 @@ namespace BoVoyageJJAN.Controllers
     public class TravelsController : BaseController
     {
         // GET: Travels
-        public ActionResult Index()
+        public ActionResult Index(TravelSearchViewModel model)
         {
             var travels = db.Trips.Include(t => t.Agency).Include(t => t.Destination);
             return View(travels.ToList());
@@ -34,27 +34,6 @@ namespace BoVoyageJJAN.Controllers
             }
             return View(trip);
         }
-
-
-        //GET: Travels/Search
-        public IQueryable<Trip> GetSearch(DateTime? departureDate = null, DateTime? returnDate = null, int? placeNumber = null, decimal? price = null, int? destinationId = null, int? agencyId = null)
-        {
-            IQueryable<Trip> liste = db.Trips;
-            if (departureDate != null)
-                liste = liste.Where(x => x.DepartureDate == returnDate);
-            if (returnDate != null)
-                liste = liste.Where(x => x.ReturnDate == returnDate);
-            if (placeNumber != null)
-                liste = liste.Where(x => x.PlaceNumber == placeNumber);
-            if (price != null)
-                liste = liste.Where(x => x.Price == price);
-            if (destinationId != null)
-                liste = liste.Where(x => x.DestinationID == destinationId);
-            if (agencyId != null)
-                liste = liste.Where(x => x.AgencyID == agencyId);
-
-            return liste;
-        }
-      
+              
     }
 }
---
+++ BoVoyageJJAN/BoVoyageJJAN/Models/TravelSearchViewModel.cs
@@ -0,0 +1,35 @@
+﻿using BoVoyageJJAN.Models;
+using BoVoyageJJAN.Utils.Validator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageJJAN.Models
+{
+    public class TravelSearchViewModel
+    {
+        [Display(Name = "Destination")]
+        
+        [StringLength(50, ErrorMessage = "Destination trop longue")]
+        public string Destination { get; set; }
+
+        [Display(Name = "Prix maximum")]
+        public decimal? MaxPrice { get; set; }
+
+        [Display(Name = "Prix minimum")]
+        public decimal? MinPrice { get; set; }
+
+        [Display(Name = "Départ après le")]
+        [DataType(DataType.Date)]
+        public DateTime? MinDate { get; set; }
+
+        [Display(Name = "Retour avant le ")]
+        [DataType(DataType.Date)]
+        public DateTime? MaxDate { get; set; }
+
+        public IEnumerable<Trip> Trips { get; set; }
+    }
+}
