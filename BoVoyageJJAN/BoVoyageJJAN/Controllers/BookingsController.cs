b24323c480a7265163fdd3ccfc94f8936aee6173
Jessy <jessy@laniel.me>
2018-07-27 10:42:48 +0200

    Refont du controller bookings et sa vue

--- BoVoyageJJAN/BoVoyageJJAN/Controllers/BookingsController.cs
+++ BoVoyageJJAN/BoVoyageJJAN/Controllers/BookingsController.cs
@@ -7,7 +7,6 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using BoVoyageJJAN.Data;
-using BoVoyageJJAN.Filter;
 using BoVoyageJJAN.Models;
 
 namespace BoVoyageJJAN.Controllers
@@ -15,13 +14,18 @@ namespace BoVoyageJJAN.Controllers
     public class BookingsController : BaseController
     {
 
+        // GET: Bookings
+        public ActionResult Index(int? id)
+        {
+            var reservations = db.Reservations.Include(r => r.Customer).Where(x => x.CustomerID == id);
+            return View(reservations.ToList());
+        }
 
         // GET: Bookings/Create
         public ActionResult Create()
         {
             ViewBag.CustomerID = new SelectList(db.Customers, "ID", "Mail");
             ViewBag.TripID = new SelectList(db.Trips, "ID", "ID");
-
             return View();
         }
 
@@ -30,65 +34,89 @@ namespace BoVoyageJJAN.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [AuthenticationUserFilter]
-        public ActionResult Create([Bind(Include = "ID,CreditCardNumber,TotalPrice,Insurance,ParticipantNumber,ParticipantUnderTwelveNumber,CreatedAt,CustomerID,TripID")] Reservation reservation)
+        public ActionResult Create([Bind(Include = "ID,CreditCardNumber,TotalPrice,Insurance,ParticipantNumber,ParticipantUnderTwelveNumber,CreatedAt,Statut,CustomerID,TripID")] Reservation reservation)
         {
-            /* var old= db.Reservations.SingleOrDefault(x => x.ID == reservation.ID);
-             reservation.CreatedAt = old.CreatedAt;
-             db.Entry(old).State = EntityState.Detached;*/
-
-
             if (ModelState.IsValid)
             {
                 db.Reservations.Add(reservation);
                 db.SaveChanges();
-                return View();
+                return RedirectToAction("Index");
             }
 
             ViewBag.CustomerID = new SelectList(db.Customers, "ID", "Mail", reservation.CustomerID);
             ViewBag.TripID = new SelectList(db.Trips, "ID", "ID", reservation.TripID);
-            
-            return View();
+            TempData["Message"] = "Réservation ajoutée";
+            return View(reservation);
         }
-        public ActionResult AddParticipant(int? id, Participant participant)
+
+        // GET: Bookings/Edit/5
+        public ActionResult Edit(int? id)
         {
-            if (ModelState.IsValid)
+            if (id == null)
             {
-
-                db.Participants.Add(participant);
-                db.SaveChanges();
-                return View();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ViewBag.Reservation = new SelectList(db.Reservations, "TotalPrice", "ParticipantNumber", "ParticipantUnderTwelveNumber", "TripID", "ID");
-            return View();
-
-
+            Reservation reservation =db.Reservations.Include(x => x.Participants).SingleOrDefault(x => x.ID == id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "Mail", reservation.CustomerID);
+            ViewBag.TripID = new SelectList(db.Trips, "ID", "ID", reservation.TripID);
+            return View(reservation);
         }
 
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Cancel([Bind(Include = "ID,CreditCardNumber,TotalPrice,Insurance,ParticipantNumber,ParticipantUnderTwelveNumber,CreatedAt,CustomerID,TripID,Statut")] Reservation reservation)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CreditCardNumber,TotalPrice,Insurance,ParticipantNumber,ParticipantUnderTwelveNumber,CreatedAt,Statut,CustomerID,TripID")] Reservation reservation)
         {
-
             if (ModelState.IsValid)
             {
                 db.Entry(reservation).State = EntityState.Modified;
-                reservation.Statut = 0;
                 db.SaveChanges();
-                return RedirectToAction("List");
+                return RedirectToAction("Index");
             }
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "Mail", reservation.CustomerID);
+            ViewBag.TripID = new SelectList(db.Trips, "ID", "ID", reservation.TripID);
+            return View(reservation);
+        }
 
-            return RedirectToAction("Index", "Home");
+        // GET: Bookings/Cancel/5
+        public ActionResult Cancel([Bind(Include = "ID,CreditCardNumber,TotalPrice,Insurance,ParticipantNumber,ParticipantUnderTwelveNumber,CreatedAt,Statut,CustomerID,TripID")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
 
-
+            db.Entry(reservation).State = EntityState.Modified;
+            reservation.Statut = 0;
+            db.SaveChanges();
+            TempData["Message"] = "Réservation annulée";
+            return RedirectToAction("Index");
 
         }
+
+
+
         [HttpPost]
-        public ActionResult List(int? id)
+        public ActionResult AddParticipant(int? id, Participant participant)
         {
-            if (id != null)
+            if (id == null)
             {
-                var liste = db.Reservations.Include(x => x.CustomerID);
-                return View(liste);
+                TempData["Message"] = "Aucun participant";
+                return RedirectToAction("Edit", new { id });
+            }
+
+            if (participant != null)
+            {
+                var person = new Participant();
+
+                db.Participants.Add(person);
+                db.SaveChanges();
+                TempData["Message"] = "participant ajouté";
+                return RedirectToAction("Edit", new { id = person.ReservationID });
+
             }
             else
             {
