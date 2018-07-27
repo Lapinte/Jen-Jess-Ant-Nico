0518b1534880ca91905afd4d842514e57b99dab2
Antoine <antoine_352@hotmail.fr>
2018-07-27 15:40:50 +0200

    Modification du HomeController pour la vue Contact Us

--- BoVoyageJJAN/BoVoyageJJAN/Controllers/HomeController.cs
+++ BoVoyageJJAN/BoVoyageJJAN/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BoVoyageJJAN.Filter;
+using BoVoyageJJAN.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,13 +16,37 @@ namespace BoVoyageJJAN.Controllers
         {
             return View();
         }
+
         public ActionResult About()
         {
             return View();
         }
+
+        [AuthenticationUserFilter]
         public ActionResult Contact()
         {
             return View();
         }
+
+        // POST: Home/Contact
+        /*[AuthenticationUserFilter]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Contact([Bind(Include = "ID,Mail")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Configuration.ValidateOnSaveEnabled = false;
+
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                DisplayMessage("Votre demande a bien été envoyée " + customer.Firstname + ". Elle sera traitée dans les meilleurs délais.", MessageType.SUCCESS);
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }*/
+
+
     }
 }
